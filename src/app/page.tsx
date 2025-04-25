db699174bcf92a0cd18c2f12df2036e4cd8e7fbf
c0ndi <kowalik0konrad@gmail.com>
2025-04-25 14:55:03 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-dvh bg-[#13120E]">
-      <Image src="/logo.png" alt="logo" width={400} height={400} />
+      <Image src="/logo.png" alt="logo" width={400} height={400} className="w-[200px] h-[200px] md:w-[400px] md:h-[400px]" />
     </div>
   );
 }
